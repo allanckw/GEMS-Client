4192af57fbcfd85734eb9ddbe467ee5618d715a1
Kai Ang <zi.kai88@gmail.com>
2012-02-18 20:28:16 +0800

    New form added
    Event Form - Add and Manage(Edit/Delete)
    
    Please do not touch the servicecontactlist form as it is not complete yet

---
+++ WpfApplication/EvMS/frmEventAdd.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gems.UIWPF
+{
+	/// <summary>
+	/// Interaction logic for frmEventAdd.xaml
+	/// </summary>
+	public partial class frmEventAdd : Window
+	{
+		public frmEventAdd()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+	}
+}
