c4a82bedc9321c6e037913f8539a40270eb1f98f
ahbeng <beng@nus.edu.sg>
2012-02-23 17:48:31 +0800

    Guest list form

---
+++ WpfApplication/LogMS/Guests/frmGuestList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gems.UIWPF
+{
+    /// <summary>
+    /// Interaction logic for frmGuestList.xaml
+    /// </summary>
+    public partial class frmGuestList : Window
+    {
+        public frmGuestList()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
