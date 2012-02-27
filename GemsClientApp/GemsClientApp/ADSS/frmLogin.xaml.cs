9eb1d2e5c1684cf94a681ccef128c7345ac8ed1b
Allan Chong <kw_chong@nus.edu.sg>
2012-02-28 02:58:24 +0800

    PREVIOUS PROJECT CORRUPTED, This is a new project created with all the source codes intact..
    There are some bugs in facility booking will fix after mid terms

---
+++ GemsClientApp/GemsClientApp/ADSS/frmLogin.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using evmsService.entities;
+
+namespace Gems.UIWPF
+{
+    public partial class frmLogin : Window
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            this.txtUserID.Focus();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.Compare(txtSvAddr.Text.Trim(), ConfigHelper.GetEndpointAddress(), true) != 0)
+            {
+                ConfigHelper.SaveEndpointAddress(txtSvAddr.Text.Trim());
+            }
+
+            WCFHelperClient client = new WCFHelperClient();
+            try
+            {
+                User u = client.login(txtUserID.Text.Trim(), txtPassword.Password);
+                var admForm = new frmMain(u, this);
+                this.Visibility = Visibility.Collapsed;
+                this.txtPassword.Clear();
+                this.txtUserID.Clear();
+                this.txtUserID.Focus();
+                admForm.Show();
+                client.Close();  // Always close the client.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtSvAddr.Text = ConfigHelper.GetEndpointAddress();
+        }
+
+    }
+}
+
