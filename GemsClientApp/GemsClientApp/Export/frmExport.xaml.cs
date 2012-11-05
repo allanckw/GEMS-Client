448c1b9c7a4ace4fcaeb7737b0f7f3e36a97c376
zikai88 <zi.kai88@gmail.com>
2012-11-05 19:56:15 +0800

    Updated stub class and project file include reference for interop
    New Plain Form frmExport for nick to do export

---
+++ GemsClientApp/GemsClientApp/Export/frmExport.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using evmsService.entities;
+
+namespace Gems.UIWPF
+{
+    public partial class frmExport : Window
+    {
+        private User user;
+        private Events event_;
+
+        public frmExport()
+        {
+            InitializeComponent();
+        }
+
+        public frmExport(User u, Events e)
+            : this()
+        {
+            this.user = u;
+            this.event_ = e;
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
+
